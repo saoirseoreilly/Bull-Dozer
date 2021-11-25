27ab204b7101a21f12239169f5e73beabe7ff7aa
saoirseoreilly <b00132517@mytudublin.ie>
2021-11-25 12:48:59 +0000

    Player Control Added

--- Bull-Dozer/Assets/Easy Primitive Animals/FarmEdition/Scripts/RotateStand.cs
+++ Bull-Dozer/Assets/Easy Primitive Animals/FarmEdition/Scripts/RotateStand.cs
@@ -2,7 +2,7 @@
 
 public class RotateStand : MonoBehaviour
 {
-    private float rotSpeed = 10f;
+    private float rotSpeed = 150f;
     void Update()
     {
         transform.Rotate(-Vector3.up * (rotSpeed * Time.deltaTime));
--- Bull-Dozer/Assets/Scripts/PlayerController.cs
+++ Bull-Dozer/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float XRange = 13;
+
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
         
     }
     
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            playerRb.AddForce(Vector3.up * 1000, ForceMode.Impulse);
+
+        }
+        if (transform.position.x < -XRange)
+        {
+            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+
+        }
+        if (transform.position.x > XRange)
+        {
+            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         
