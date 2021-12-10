8464ec660f75f871cdcda2690aaac710fbae2584
saoirseoreilly <b00132517@mytudublin.ie>
2021-12-10 13:35:04 +0000

    New repository final version

---
+++ Bull-Dozer/Assets/Easy Primitive Animals/FarmEdition/Scripts/RotateStand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotateStand : MonoBehaviour
+{
+    private float rotSpeed = 150f;
+    void Update()
+    {
+        transform.Rotate(-Vector3.up * (rotSpeed * Time.deltaTime));
+    }
+}
